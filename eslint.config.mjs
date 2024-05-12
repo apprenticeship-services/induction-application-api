1b144adb84918bf4bb63ae3693f64dbc766039bd
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-12 20:09:17 +0100

    chore: project configuration - TS - Jest - EsLint - Husky - Lint-Staged - CommitLinter

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+
+export default [
+  ...compat.extends('eslint-config-standard'),
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    },
+    files: ['src/**/*.js', 'src/**/*.ts'],
+    languageOptions: {
+      globals: globals.browser,
+      sourceType: 'commonjs',
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  }
+]
---
+++ jest.config.js
@@ -0,0 +1,29 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!**/test/**'],
+  coveragePathIgnorePatterns: [
+    '.+-protocols.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  roots: [
+    '<rootDir>/src'
+  ],
+  transform: {
+    '.(ts)': 'ts-jest'
+  },
+  // preset: '@shelf/jest-mongodb',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
+
+module.exports = config
