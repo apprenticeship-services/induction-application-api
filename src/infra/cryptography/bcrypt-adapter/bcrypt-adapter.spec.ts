c0555c7f572c6a556f99f5276068bb69990854b3
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-13 22:52:12 +0100

    test: ensure BcryptAdapter returns hashed value on success

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -5,7 +5,7 @@ const salt = 12
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
-    return Promise.resolve('hashed_password')
+    return Promise.resolve('hashed_value')
   }
 }))
 
@@ -24,5 +24,11 @@ describe('BcryptAdapter', () => {
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => Promise.reject(new Error()))
     const hashedValue = sut.hash(fakeValue())
     expect(hashedValue).rejects.toThrow()
+  })
+
+  test('Should return hashed value on success', async () => {
+    const sut = new BcryptAdapter()
+    const hashedValue = await sut.hash(fakeValue())
+    expect(hashedValue).toBe('hashed_value')
   })
 })
