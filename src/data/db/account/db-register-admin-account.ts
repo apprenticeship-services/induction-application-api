3bd2c2b9eaabd3230322e53f7a820f5859206f50
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-13 18:06:32 +0100

    feat: ensure DbRegisterAdminAccount calls Hasher with correct generated password value

--- src/data/db/account/db-register-admin-account.ts
+++ src/data/db/account/db-register-admin-account.ts
@@ -2,19 +2,24 @@ import { RegisterAccountRepository } from '@/data/protocols/db/register-account-
 import { AccountModel } from '@/domain/models/account'
 import { RegisterAccount, RegisterAccountParams } from '@/domain/use-cases/register-account'
 import { Generator } from '@/data/protocols/cryptography/generator'
+import { Hasher } from '@/data/protocols/cryptography/hasher'
 
 export class DbRegisterAdminAccount implements RegisterAccount {
   private readonly role = 'admin'
   constructor (
-    private readonly registerAccountRepository: RegisterAccountRepository,
-    private readonly passwordGenerator: Generator) {
-    this.registerAccountRepository = registerAccountRepository
+    private readonly passwordGenerator: Generator,
+    private readonly hasher: Hasher,
+    private readonly registerAccountRepository: RegisterAccountRepository
+  ) {
     this.passwordGenerator = passwordGenerator
+    this.hasher = hasher
+    this.registerAccountRepository = registerAccountRepository
   }
 
   async register (credentials: RegisterAccountParams): Promise<AccountModel> {
     const password = this.passwordGenerator.generate()
-    const account = await this.registerAccountRepository.register({ ...credentials, password, role: this.role })
+    const hashedPassword = await this.hasher.hash(password)
+    const account = await this.registerAccountRepository.register({ ...credentials, password: hashedPassword, role: this.role })
     return null
   }
 }
