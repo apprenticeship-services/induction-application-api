26800a205b42a9b39169d996429d67bf0dbe3801
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-13 14:23:26 +0100

    test: ensure RegisterAdminController calls RegisterAdmin usecase with correct values

---
+++ src/presentation/controllers/account/register-admin-controller.spec.ts
@@ -0,0 +1,52 @@
+import { HttpRequest } from '@/presentation/protocols'
+import { RegisterAdminAccountController } from './register-admin-controller'
+import { RegisterAdminAccount, RegisterAdminAccountParams } from '@/domain/use-cases/register-admin-account'
+import { AccountModel } from '@/domain/models/account'
+
+type Sut = {
+    sut: RegisterAdminAccountController,
+    registerAdminAccountStub: any
+}
+
+const makeSut = (): Sut => {
+  const registerAdminAccountStub = makeRegisterAdminAccountSub()
+  const sut = new RegisterAdminAccountController(registerAdminAccountStub)
+  return {
+    sut,
+    registerAdminAccountStub
+  }
+}
+
+const makeRegisterAdminAccountSub = (): RegisterAdminAccount => {
+  class RegisterAdminAccountStub implements RegisterAdminAccount {
+    async register (credentials: RegisterAdminAccountParams): Promise<AccountModel> {
+      return Promise.resolve(fakeAccountModel())
+    }
+  }
+  return new RegisterAdminAccountStub()
+}
+
+const fakeRequest = (): HttpRequest => ({
+  body: fakeAdminAccount()
+})
+
+const fakeAdminAccount = () => ({
+  name: 'any_name',
+  email: 'any_email@hotmail.com'
+})
+
+const fakeAccountModel = (): AccountModel => ({
+  _id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@hotmail.com',
+  password: 'any_password'
+})
+
+describe('RegisterAdminController', () => {
+  test('Should Call RegisterAdminAccount with correct values', async () => {
+    const { sut, registerAdminAccountStub } = makeSut()
+    const registerSpy = jest.spyOn(registerAdminAccountStub, 'register')
+    await sut.handle(fakeRequest())
+    expect(registerSpy).toHaveBeenCalledWith(fakeAdminAccount())
+  })
+})
