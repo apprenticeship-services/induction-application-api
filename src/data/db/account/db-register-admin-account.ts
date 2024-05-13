67833179bddd4a6e0513849fdde2c6a0d9b3efe7
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-13 18:13:33 +0100

    feat: ensure DbRegisterAdminAccount calls LoadAccountByEmailRepository  with correct email

--- src/data/db/account/db-register-admin-account.ts
+++ src/data/db/account/db-register-admin-account.ts
@@ -3,20 +3,24 @@ import { AccountModel } from '@/domain/models/account'
 import { RegisterAccount, RegisterAccountParams } from '@/domain/use-cases/register-account'
 import { Generator } from '@/data/protocols/cryptography/generator'
 import { Hasher } from '@/data/protocols/cryptography/hasher'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository'
 
 export class DbRegisterAdminAccount implements RegisterAccount {
   private readonly role = 'admin'
   constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly passwordGenerator: Generator,
     private readonly hasher: Hasher,
     private readonly registerAccountRepository: RegisterAccountRepository
   ) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.passwordGenerator = passwordGenerator
     this.hasher = hasher
     this.registerAccountRepository = registerAccountRepository
   }
 
   async register (credentials: RegisterAccountParams): Promise<AccountModel> {
+    const isAccount = this.loadAccountByEmailRepository.loadByEmail(credentials.email)
     const password = this.passwordGenerator.generate()
     const hashedPassword = await this.hasher.hash(password)
     const account = await this.registerAccountRepository.register({ ...credentials, password: hashedPassword, role: this.role })
