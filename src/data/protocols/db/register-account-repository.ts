b0064fd0afe32b5e66b2610178d57380d123861a
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-14 13:56:22 +0100

    refactor: decided to implement date from system inside the use case implemented by data layer

--- src/data/protocols/db/register-account-repository.ts
+++ src/data/protocols/db/register-account-repository.ts
@@ -1,6 +1,6 @@
 import { AccountModel } from '@/domain/models/account'
 
-export type RegisterAccountRepositoryParams = Omit<AccountModel, '_id' | 'createdAt'>
+export type RegisterAccountRepositoryParams = Omit<AccountModel, '_id'>
 
 export interface RegisterAccountRepository{
     register(credentials: RegisterAccountRepositoryParams): Promise<AccountModel>
