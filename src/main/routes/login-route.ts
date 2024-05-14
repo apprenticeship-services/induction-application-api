a9cf3d22f79d96b705d316e0b6e12bcb4cd84043
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-14 16:26:03 +0100

    feat: create Register Admin Route

--- src/main/routes/login-route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-export default (router: Router) => {
-  router.get('/hello', (req, res) => {
-    res.status(200).send({
-      name: 'test'
-    })
-  })
-}
---
+++ src/main/routes/register-route.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { expressRouteAdapter } from '../adapters/express/express-route-adapter'
+import { registerAdminAccountControllerFactory } from '../factories/controller/account/register-admin-account/register-admin-account-controller-factory'
+
+export default (router: Router) => {
+  router.post('/register/admin', expressRouteAdapter(registerAdminAccountControllerFactory()))
+}
