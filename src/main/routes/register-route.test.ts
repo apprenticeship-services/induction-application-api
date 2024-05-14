a83bdd031bf1ff7ed37405f05dd9bab8a8dee8c9
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-14 16:28:59 +0100

    test: ensure RegisterAdminRoute register new admin account and returns no content

---
+++ src/main/routes/register-route.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let accountsCollection: Collection
+
+describe('Register Admin Route', () => {
+  beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
+  afterAll(async () => await MongoHelper.disconnect())
+  beforeEach(async () => {
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+  })
+  describe('Register admin route', () => {
+    describe('POST /register/admin', () => {
+      test('Should register an admin account and return no content', async () => {
+        await request(app)
+          .post('/api/register/admin')
+          .send({
+            name: 'admin_name',
+            email: 'admin_email@hotmail.com'
+          })
+          .expect(204)
+      })
+    })
+  })
+})
