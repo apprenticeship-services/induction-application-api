8933170d9edd7519408445edd5e55f91145f6aa5
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-14 11:14:39 +0100

    test: ensure ValidatorComposite returns invalid param error on email invalid

--- src/validator/validators/validation-composite.spec.ts
+++ src/validator/validators/validation-composite.spec.ts
@@ -1,6 +1,7 @@
 import { Validator } from '@/presentation/protocols/validator'
 import { ValidatorComposite } from './validation-composite'
 import { MissingParamError } from '@/presentation/errors/missing-param'
+import { InvalidParamError } from '@/presentation/errors/invalid-params'
 
 type SutTypes = {
     sut: ValidatorComposite,
@@ -33,5 +34,12 @@ describe('Validator Composite', () => {
     jest.spyOn(validatorsStub[0], 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const isError = sut.validate(fakeInput())
     expect(isError).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should return invalid email error if validator fails', () => {
+    const { sut, validatorsStub } = makeSut()
+    jest.spyOn(validatorsStub[1], 'validate').mockReturnValueOnce(new InvalidParamError('email'))
+    const isError = sut.validate(fakeInput())
+    expect(isError).toEqual(new InvalidParamError('email'))
   })
 })
