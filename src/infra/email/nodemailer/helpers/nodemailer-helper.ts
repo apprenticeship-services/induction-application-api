b6315791d4da814480c29fca6a0153a075233b96
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-15 12:27:41 +0100

    fix: add secure flag on nodemailer transporter

--- src/infra/email/nodemailer/helpers/nodemailer-helper.ts
+++ src/infra/email/nodemailer/helpers/nodemailer-helper.ts
@@ -6,6 +6,7 @@ export class NodemailerHelper {
   static initMail () {
     this.transporter = nodemailer.createTransport({
       service: env.emailService,
+      secure: false,
       auth: {
         user: env.emailServiceUser,
         pass: env.emailServiceSecret
