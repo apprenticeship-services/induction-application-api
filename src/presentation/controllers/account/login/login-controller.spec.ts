87ca7688150e6dc5d722c222ad8ed15cc5668011
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-16 14:34:09 +0100

    test: ensure LoginController calls Authentication auth method with correct values

--- src/presentation/controllers/account/login/login-controller.spec.ts
+++ src/presentation/controllers/account/login/login-controller.spec.ts
@@ -1,23 +1,26 @@
 import { Validator } from '@/presentation/protocols/validator'
 import { LoginController } from './login-controller'
-import { AuthenticationParams } from '@/domain/use-cases/authentication'
 import { HttpRequest } from '@/presentation/protocols'
 import { ValidatorComposite } from '@/validator/validators/validation-composite'
 import { MissingParamError } from '@/presentation/errors/missing-param'
 import { badRequest } from '@/presentation/helpers/http-helper'
-import { InvalidParamError } from '@/presentation/errors/invalid-params'
+import { Authentication, AuthenticationParams } from '@/domain/use-cases/authentication'
+import { UserCredentials } from '@/domain/models/user-credentials'
 
 type Sut = {
     sut: LoginController
     validatorStub: Validator
+    authenticationStub: Authentication
 }
 
 const makeSut = (): Sut => {
   const validatorStub = new ValidatorComposite([makeValidatorStub()])
-  const sut = new LoginController(validatorStub)
+  const authenticationStub = makeAuthenticationStub()
+  const sut = new LoginController(validatorStub, authenticationStub)
   return {
     sut,
-    validatorStub
+    validatorStub,
+    authenticationStub
   }
 }
 
@@ -30,11 +33,32 @@ const makeValidatorStub = (): Validator => {
   return new ValidatorStub()
 }
 
+const makeAuthenticationStub = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    auth (authCredentials: AuthenticationParams): Promise<UserCredentials> {
+      return Promise.resolve(fakeUserCredentials())
+    }
+  }
+  return new AuthenticationStub()
+}
+
 const fakeRequest = (): HttpRequest => ({
   body: {
     email: 'any_email@hotmail.com',
     password: 'any_password'
   }
+})
+
+const fakeAuthCredentials = (): AuthenticationParams => ({
+  email: 'any_email@hotmail.com',
+  password: 'any_password'
+})
+
+const fakeUserCredentials = (): UserCredentials => ({
+  name: 'any_name',
+  email: 'any_email@hotmail.com',
+  role: 'any_role',
+  accessToken: 'any_token'
 })
 
 describe('Login Controller', () => {
@@ -52,10 +76,17 @@ describe('Login Controller', () => {
     expect(response).toEqual(badRequest(new MissingParamError('email')))
   })
 
-  test('Should return 400 if name is not provided', async () => {
+  test('Should return 400 if password is not provided', async () => {
     const { sut, validatorStub } = makeSut()
     jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(new MissingParamError('password'))
     const response = await sut.handle(fakeRequest())
     expect(response).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should call Authentication with correct values', async () => {
+    const { sut, authenticationStub } = makeSut()
+    const authSpy = jest.spyOn(authenticationStub, 'auth')
+    await sut.handle(fakeRequest())
+    expect(authSpy).toHaveBeenCalledWith(fakeAuthCredentials())
   })
 })
