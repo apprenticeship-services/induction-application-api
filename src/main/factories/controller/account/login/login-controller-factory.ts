783b84d604102ac7a76c1903c139f35bd6f3ea04
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-16 16:11:26 +0100

    feat: create LoginController factory

---
+++ src/main/factories/controller/account/login/login-controller-factory.ts
@@ -0,0 +1,15 @@
+import { LoginController } from '@/presentation/controllers/account/login/login-controller'
+import { loginValidatorFactory } from './login-validator-factory'
+import { DbAuthentication } from '@/data/db/authentication/db-authentication'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '@/infra/cryptography/jwt-adapter/jwt-adapter'
+import env from '@/main/config/env'
+
+export const loginControllerFactory = (): LoginController => {
+  const loadAccountByEmailRepository = new AccountMongoRepository()
+  const hashComparer = new BcryptAdapter()
+  const encrypter = new JwtAdapter(env.jwtSecretToken)
+  const authentication = new DbAuthentication(loadAccountByEmailRepository, hashComparer, encrypter)
+  return new LoginController(loginValidatorFactory(), authentication)
+}
