48cc9fb175a847481ac1a4f21e61a85096a2f16e
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-16 16:18:30 +0100

    test: ensure /login route returns 401 if email is not registered

--- src/main/routes/login-route.test.ts
+++ src/main/routes/login-route.test.ts
@@ -1,8 +1,10 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
+import { Collection, ObjectId } from 'mongodb'
+import { AccountModel } from '@/domain/models/account'
 
-let accountsCollection
+let accountsCollection: Collection
 describe('Login Route', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
   afterAll(async () => await MongoHelper.disconnect())
@@ -16,6 +18,24 @@ describe('Login Route', () => {
         .post('/api/login')
         .send({})
         .expect(400)
+    })
+
+    test('Should return 401 if email is not registered', async () => {
+      await accountsCollection.insertOne({
+        name: 'test_name',
+        email: 'test_email@hotmail.com',
+        role: 'test_role',
+        password: 'test_password',
+        createdAt: new Date()
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'email_not_registered@hotmail.com',
+          password: 'any_password'
+        })
+        .expect(401)
     })
   })
 })
