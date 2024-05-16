078e3850f20dadc85a9602a90e4389a3cdb2a601
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-16 17:12:34 +0100

    refactor: delete custom modules as it is not needed now

--- src/main/config/custom-modules.d.ts
+++
@@ -1,8 +0,0 @@
-import { JwtPayload as OriginalJwtPayload } from 'jsonwebtoken'
-
-declare module 'jsonwebtoken' {
-   interface JwtPayload extends OriginalJwtPayload {
-      _id?: string;
-      role?:string
-  }
-}
