90f53647029096e3b90d7e5e65bb81897bc0bc44
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-16 11:54:51 +0100

    feat: ensure DbAuthentication calls HashComparer dep with correct password and hashed password

--- src/data/db/authentication/db-authentication.ts
+++ src/data/db/authentication/db-authentication.ts
@@ -1,20 +1,25 @@
+import { HashComparer } from '@/data/protocols/cryptography/hash-comparer'
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository'
 import { UserCredentials } from '@/domain/models/user-credentials'
 import { Authentication, AuthenticationParams } from '@/domain/use-cases/authentication'
 
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
+    this.hashComparer = hashComparer
   }
 
   async auth (userCredentials: AuthenticationParams): Promise<UserCredentials> {
-    const { email } = userCredentials
+    const { email, password } = userCredentials
     const isAccount = await this.loadAccountByEmailRepository.loadByEmail(email)
     if (!isAccount) {
       return null
     }
+
+    const isPasswordValid = await this.hashComparer.compare(password, isAccount.password)
 
     // check if password matches with ghashed password, else null
     // create jwt token
