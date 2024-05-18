02c5187e21798292e5f36cdba2a0be8b019d0e52
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-18 14:53:03 +0100

    feat: ensure RegisterapprenticeController returns 400 on validator fail

--- src/presentation/controllers/account/apprentices/register-apprentice-controller.ts
+++ src/presentation/controllers/account/apprentices/register-apprentice-controller.ts
@@ -1,4 +1,5 @@
 import { RegisterApprenticeAccount } from '@/domain/use-cases/register-apprentice-account'
+import { badRequest } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { Validator } from '@/presentation/protocols/validator'
 
@@ -12,6 +13,8 @@ export class RegisterApprenticeController implements Controller {
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validate(request.body)
-    return null
+    if (error) {
+      return badRequest(error)
+    }
   }
 }
