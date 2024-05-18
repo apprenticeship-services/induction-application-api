fa392191457273fb568503556ad8990d8e5146ad
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-18 18:50:16 +0100

    test: ensure TemplateGenerator generates registration email template for apprentices

--- src/infra/email/nodemailer/template-generator/template-email-generator.spec.ts
+++ src/infra/email/nodemailer/template-generator/template-email-generator.spec.ts
@@ -6,11 +6,24 @@ const fakeAdminAccount = () => ({
   password: 'any_password',
   role: 'admin'
 })
+const fakeApprenticeAccount = () => ({
+  name: 'any_name',
+  emailTo: 'any_email@hotmail.com',
+  password: 'any_password',
+  role: 'apprentice'
+})
 describe('Template Email Generator', () => {
   describe('Registration Email Template', () => {
     test('Should return a header and content if account is for admin', () => {
       const sut = new TemplateEmailGenerator()
       const template = sut.generateRegistrationEmailTemplate(fakeAdminAccount())
+      expect(template.header).toBeDefined()
+      expect(template.content).toBeDefined()
+    })
+
+    test('Should return a header and content if account is for apprentice', () => {
+      const sut = new TemplateEmailGenerator()
+      const template = sut.generateRegistrationEmailTemplate(fakeApprenticeAccount())
       expect(template.header).toBeDefined()
       expect(template.content).toBeDefined()
     })
