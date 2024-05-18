98450b260777896ca3065fa50619af3b0c6a4f70
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-18 18:29:11 +0100

    test: ensures POST /apprentices returns 403 if email is already registered

--- src/main/routes/apprentices-route.test.ts
+++ src/main/routes/apprentices-route.test.ts
@@ -3,6 +3,8 @@ import app from '../config/app'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
 import { EmailServiceAdapter } from '@/infra/email/nodemailer/email-service/email-service-adapter'
+import { forbidden } from '@/presentation/helpers/http-helper'
+import { AlreadyExists } from '@/presentation/errors/already-exists'
 
 let accountsCollection: Collection
 
@@ -26,6 +28,23 @@ describe('Register Admin Route', () => {
             advisor: 'apprentice_advisor'
           })
           .expect(204)
+      })
+
+      test('Should return 403 if apprentice email is already registered', async () => {
+        await accountsCollection.insertOne({
+          name: 'apprentice_name',
+          email: 'apprentice_email@hotmail.com'
+        })
+
+        await request(app)
+          .post('/api/apprentices')
+          .send({
+            name: 'apprentice_name',
+            email: 'apprentice_email@hotmail.com',
+            trade: 'apprentice_trade',
+            advisor: 'apprentice_advisor'
+          })
+          .expect(403)
       })
     })
   })
