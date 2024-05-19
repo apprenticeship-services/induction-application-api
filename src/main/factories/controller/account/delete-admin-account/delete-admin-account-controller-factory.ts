c2dface86676e33eca71eef3d5c00c0ed45f8b85
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-19 15:38:29 +0100

    feat: create DeleteAdminController factory

---
+++ src/main/factories/controller/account/delete-admin-account/delete-admin-account-controller-factory.ts
@@ -0,0 +1,16 @@
+import { DbDeleteAdminAccountById } from '@/data/use-cases/db/account/db-delete-admin-account'
+import { DbLoadAccountById } from '@/data/use-cases/db/account/db-load-account-by-id'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
+import { IdParamValidatorAdapter } from '@/infra/validator/id-param-validator-adapter/mongodb/id-param-validator'
+import { DeleteAdminController } from '@/presentation/controllers/account/admins/delete-admin-controller'
+import { Controller } from '@/presentation/protocols'
+import { IdParamValidation } from '@/validator/validators/id-param-validator'
+
+export const deleteAdminControllerFactory = (): Controller => {
+  const idParamValidator = new IdParamValidation('id', new IdParamValidatorAdapter())
+  const loadAccountByIdRepository = new AccountMongoRepository()
+  const loadAccountById = new DbLoadAccountById(loadAccountByIdRepository)
+  const deleteAccountByIdRepository = new AccountMongoRepository()
+  const deleteAccountById = new DbDeleteAdminAccountById(deleteAccountByIdRepository)
+  return new DeleteAdminController(idParamValidator, loadAccountById, deleteAccountById)
+}
