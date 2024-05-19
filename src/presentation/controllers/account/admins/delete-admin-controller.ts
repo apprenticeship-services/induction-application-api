d9fc053c3a4360f214ca2eba0173b083092224f1
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-19 15:23:59 +0100

    feat: ensure DeleteAdminController calls IdParamValidator with correct values

--- src/presentation/controllers/account/admins/delete-admin-controller.ts
+++ src/presentation/controllers/account/admins/delete-admin-controller.ts
@@ -2,19 +2,25 @@ import { DeleteAccountById } from '@/domain/use-cases/delete-account-by-id'
 import { LoadAccountById } from '@/domain/use-cases/load-account-by-id'
 import { AccountNotFoundError } from '@/presentation/errors/account-not-found-error'
 import { DeleteError } from '@/presentation/errors/delete-error'
-import { noContent, notFound, serverError, success } from '@/presentation/helpers/http-helper'
+import { badRequest, noContent, notFound, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Validator } from '@/presentation/protocols/validator'
 
 export class DeleteAdminController implements Controller {
   constructor (
+        private readonly idParamValidator: Validator,
         private readonly loadAccountById: LoadAccountById,
-      private readonly deleteAccountById: DeleteAccountById) {
+        private readonly deleteAccountById: DeleteAccountById) {
     this.loadAccountById = loadAccountById
     this.deleteAccountById = deleteAccountById
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.idParamValidator.validate(request.params)
+      if (error) {
+        return badRequest(error)
+      }
       const { id } = request.params
       const account = await this.loadAccountById.loadById(id)
       if (!account) {
