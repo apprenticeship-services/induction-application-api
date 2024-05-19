1aa32f84fcd49829ec2e255223ae456c0a2bc2c3
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-19 16:50:59 +0100

    feat: create /admins/:id route

--- src/main/routes/admins-route.ts
+++ src/main/routes/admins-route.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { expressRouteAdapter } from '../adapters/express/express-route-adapter'
 import { registerAdminAccountControllerFactory } from '../factories/controller/account/register-admin-account/register-admin-account-controller-factory'
+import { deleteAdminControllerFactory } from '../factories/controller/account/delete-admin-account/delete-admin-account-controller-factory'
 
 export default (router: Router) => {
   router.post('/admins', expressRouteAdapter(registerAdminAccountControllerFactory()))
+  router.delete('/admins/:id', expressRouteAdapter(deleteAdminControllerFactory()))
 }
