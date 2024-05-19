ba3a756f55ee635339946e811062d2c853cde7db
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-19 15:15:47 +0100

    test: ensure IdParamValidatorAdapter returns true if ObjectId returns true

---
+++ src/infra/validator/id-param-validator-adapter/mongodb/id-param-validator-adapter.spec.ts
@@ -0,0 +1,22 @@
+import { ObjectId } from 'mongodb'
+import { IdParamValidatorAdapter } from './id-param-validator'
+
+const fakeId = () => new ObjectId().toString()
+
+describe('IdParamValidatorAdapter', () => {
+  test('Should call ObjectId method with correct value', () => {
+    const sut = new IdParamValidatorAdapter()
+    const objectIdSpy = jest.spyOn(ObjectId, 'isValid')
+    const id = fakeId()
+    sut.isValid(id)
+    expect(objectIdSpy).toHaveBeenCalledWith(id)
+  })
+
+  test('Should return true if ObjectId method returns true', () => {
+    const sut = new IdParamValidatorAdapter()
+    jest.spyOn(ObjectId, 'isValid').mockReturnValueOnce(true)
+    const id = fakeId()
+    const result = sut.isValid(id)
+    expect(result).toBe(true)
+  })
+})
