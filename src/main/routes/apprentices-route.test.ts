6373b00ad1102d833a7a64ecac08af66d034b59d
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-20 13:41:12 +0100

    test: ensure /apprentices/:id delete route delete account and apprentice information linked to it

--- src/main/routes/apprentices-route.test.ts
+++ src/main/routes/apprentices-route.test.ts
@@ -1,21 +1,26 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { EmailServiceAdapter } from '@/infra/email/nodemailer/email-service/email-service-adapter'
 import { forbidden } from '@/presentation/helpers/http-helper'
 import { AlreadyExists } from '@/presentation/errors/already-exists'
 import { MongoDbTransactionManager } from '@/infra/db/mongodb/transaction/mongodb-transaction-manager'
 
 let accountsCollection: Collection
+let apprenticesCollection: Collection
 
 describe('Register Admin Route', () => {
   beforeAll(async () => await MongoHelper.connect(process.env.MONGO_URL))
   afterAll(async () => await MongoHelper.disconnect())
   beforeEach(async () => {
     accountsCollection = await MongoHelper.getCollection('accounts')
+    apprenticesCollection = await MongoHelper.getCollection('apprentices')
   })
-  afterEach(async () => await accountsCollection.deleteMany({}))
+  afterEach(async () => {
+    await accountsCollection.deleteMany({})
+    await apprenticesCollection.deleteMany({})
+  })
   describe('Register apprentice route', () => {
     describe('POST /apprentices', () => {
       jest.spyOn(EmailServiceAdapter.prototype, 'sendRegistrationMail').mockReturnValueOnce(Promise.resolve(null))
@@ -66,6 +71,25 @@ describe('Register Admin Route', () => {
             advisor: 'apprentice_advisor'
           })
           .expect(500)
+      })
+    })
+
+    describe('DELETE /apprentices/:id', () => {
+      test('Should return 204 on success', async () => {
+        const account = await accountsCollection.insertOne({
+          name: 'apprentice_name',
+          email: 'apprentice_email@hotmail.com',
+          role: 'apprentice'
+        })
+
+        const accountId = account.insertedId.toString()
+
+        await apprenticesCollection.insertOne({
+          accountId: new ObjectId(accountId)
+        })
+        await request(app)
+          .delete(`/api/apprentices/${accountId}`)
+          .expect(204)
       })
     })
   })
