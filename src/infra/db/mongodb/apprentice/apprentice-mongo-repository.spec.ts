5cde3d3786411d2659622c92f4ec3d97892e8714
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-20 12:44:41 +0100

    test: ensure ApprenticeMongoRepository delete apprentice document on success

--- src/infra/db/mongodb/apprentice/apprentice-mongo-repository.spec.ts
+++ src/infra/db/mongodb/apprentice/apprentice-mongo-repository.spec.ts
@@ -1,12 +1,14 @@
-import { Collection, ObjectId, WithId } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { ApprenticeMongoRepository } from './apprentice-mongo-repository'
 import { ApprenticeModel } from '@/domain/models/apprentice-model'
 
-let apprenticesCollection:Collection
+let apprenticesCollection: Collection
+let accountsCollection: Collection
 describe('ApprenticeMongoRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
+    accountsCollection = await MongoHelper.getCollection('accounts')
     apprenticesCollection = await MongoHelper.getCollection('apprentices')
   })
 
@@ -15,25 +17,46 @@ describe('ApprenticeMongoRepository', () => {
   })
 
   afterEach(async () => {
+    await accountsCollection.deleteMany({})
     await apprenticesCollection.deleteMany({})
   })
 
-  test('Should register new Apprentice Document on success', async () => {
-    const { insertedId } = await (await MongoHelper.getCollection('accounts')).insertOne({
-      email: 'any_email@hotmail.com'
-    })
-    const sut = new ApprenticeMongoRepository()
-    await sut.register({
-      accountId: insertedId.toString(),
-      advisor: 'any_advisor',
-      trade: 'any_trade',
-      induction: false,
-      assessment: false,
-      updatedAt: null
-    })
+  describe('METHOD: register()', () => {
+    test('Should register new Apprentice Document on success', async () => {
+      const { insertedId } = await accountsCollection.insertOne({
+        email: 'any_email@hotmail.com'
+      })
+      const sut = new ApprenticeMongoRepository()
+      await sut.register({
+        accountId: insertedId.toString(),
+        advisor: 'any_advisor',
+        trade: 'any_trade',
+        induction: false,
+        assessment: false,
+        updatedAt: null
+      })
 
-    const document = await apprenticesCollection.find<ApprenticeModel>({ accountId: new ObjectId(insertedId.toString()) }).next()
-    expect(document).toBeDefined()
-    expect(document.accountId.toString()).toBe(insertedId.toString())
+      const document = await apprenticesCollection.findOne<ApprenticeModel>({ accountId: new ObjectId(insertedId.toString()) })
+      expect(document).toBeDefined()
+      expect(document.accountId.toString()).toBe(insertedId.toString())
+    })
+  })
+
+  describe('METHOD: deleteById()', () => {
+    test('Should delete apprentice document on success', async () => {
+      const account = await accountsCollection.insertOne({
+        email: 'any_email@hotmail.com',
+        role: 'apprentice'
+      })
+
+      await apprenticesCollection.insertOne({
+        accountId: new ObjectId(account.insertedId.toString())
+      })
+      const sut = new ApprenticeMongoRepository()
+      await sut.deleteById(account.insertedId.toString())
+
+      const apprenticeDocument = await apprenticesCollection.findOne<ApprenticeModel>({ accountId: new ObjectId(account.insertedId.toString()) })
+      expect(apprenticeDocument).toBeFalsy()
+    })
   })
 })
