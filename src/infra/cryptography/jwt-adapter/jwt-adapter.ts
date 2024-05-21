98354cf81ae5d0fe89d6e63b2be9e6ff980d97e2
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-21 20:59:47 +0100

    feat: implement JwtAdapter Decrypter interface

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,7 +1,8 @@
+import { AccountTokenPayload, Decrypter } from '@/data/protocols/cryptography/decrypter'
 import { EncryptDetails, Encrypter } from '@/data/protocols/cryptography/encrypter'
 import env from '@/main/config/env'
 import jwt from 'jsonwebtoken'
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {
     this.secret = secret
   }
@@ -9,5 +10,10 @@ export class JwtAdapter implements Encrypter {
   async encrypt (details: EncryptDetails): Promise<string> {
     const accessToken = await jwt.sign(details, this.secret)
     return accessToken
+  }
+
+  async decrypt (token: string): Promise<AccountTokenPayload> {
+    const payload = await jwt.verify(token, this.secret)
+    return payload as AccountTokenPayload
   }
 }
