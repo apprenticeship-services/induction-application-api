41bb15d4b8b059dbf0e47f3c19c0df78e9989a8f
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-21 13:23:09 +0100

    test: ensure DbUpdateApprenticeInduction calls UpdateApprenticeInductionRepo with correct values

---
+++ src/data/use-cases/db/apprentice-induction/db-update-apprentice-induction.spec.ts
@@ -0,0 +1,44 @@
+import { UpdateApprenticeInductionRepository, UpdateApprenticeInductionRepositoryParams } from '@/data/protocols/db/update-apprentice-induction-repository'
+import { DbUpdateApprenticeInduction } from './db-update-apprentice-induction'
+import MockDate from 'mockdate'
+
+type Sut = {
+  sut: DbUpdateApprenticeInduction,
+  updateApprenticeInductionRepositoryStub: UpdateApprenticeInductionRepository
+}
+
+const makeSut = (): Sut => {
+  const updateApprenticeInductionRepositoryStub = makeUpdateApprenticeInductionRepositoryStub()
+  const sut = new DbUpdateApprenticeInduction(updateApprenticeInductionRepositoryStub)
+  return {
+    sut,
+    updateApprenticeInductionRepositoryStub
+  }
+}
+
+const makeUpdateApprenticeInductionRepositoryStub = (): UpdateApprenticeInductionRepository => {
+  class UpdateApprenticeInductionRepositoryStub implements UpdateApprenticeInductionRepository {
+    updateInduction (data: UpdateApprenticeInductionRepositoryParams): Promise<void> {
+      return Promise.resolve()
+    }
+  }
+  return new UpdateApprenticeInductionRepositoryStub()
+}
+
+const fakeAccountId = 'any_accountId'
+
+const fakeUpdateInductionData = (): UpdateApprenticeInductionRepositoryParams => ({
+  accountId: 'any_accountId',
+  updatedAt: new Date()
+})
+
+describe('DbUpdateApprenticeInduction', () => {
+  beforeEach(() => MockDate.set(new Date()))
+  afterEach(() => MockDate.reset())
+  test('Should call UpdateApprenticeInductionRepository with correct values', async () => {
+    const { sut, updateApprenticeInductionRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(updateApprenticeInductionRepositoryStub, 'updateInduction')
+    await sut.updateInduction(fakeAccountId)
+    expect(updateSpy).toHaveBeenCalledWith(fakeUpdateInductionData())
+  })
+})
