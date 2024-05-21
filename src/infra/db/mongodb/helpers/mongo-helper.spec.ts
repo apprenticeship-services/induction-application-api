928511bccf4c5d925f0a2bed922fc8dccf522433
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-21 14:13:18 +0100

    test: implement mapObjetId method from MongoHelper to convert any ObjectId to string

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb'
 import { MongoHelper as sut } from './mongo-helper'
 
 describe('', () => {
@@ -9,5 +10,27 @@ describe('', () => {
     await sut.disconnect()
     const randomCollection = await sut.getCollection('example')
     expect(randomCollection).toBeTruthy()
+  })
+
+  describe('mapObjectId', () => {
+    test('should convert any new ObjectId to string', async () => {
+      const randomCollection = await sut.getCollection('example')
+      await randomCollection.insertOne({
+        _id: new ObjectId(),
+        otherId: new ObjectId(),
+        stringId: 'string_id'
+      })
+
+      type TestIdDoc = {
+        _id: string,
+        otherId: string,
+        stringId: string
+      }
+      const document = await randomCollection.findOne({ stringId: 'string_id' })
+      const mappedObj = sut.mapObjectId<TestIdDoc>(document)
+      expect(typeof mappedObj._id).toBe('string')
+      expect(typeof mappedObj.otherId).toBe('string')
+      expect(typeof mappedObj.stringId).toBe('string')
+    })
   })
 })
