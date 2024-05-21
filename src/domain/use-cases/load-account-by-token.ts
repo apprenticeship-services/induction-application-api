67c719cb5d3c08bfd4e27846ba5b354d6f3c655b
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-21 19:25:28 +0100

    feat: ensure AuthMiddleware returns 403 if no token is provided on request

---
+++ src/domain/use-cases/load-account-by-token.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../models/account'
+
+export interface LoadAccountByToken {
+    loadByToken(token:string, rolePermission?: string):Promise<AccountModel>
+}
---
+++ src/presentation/middlewares/auth/auth-middleware.ts
@@ -0,0 +1,31 @@
+import { Middleware } from '@/presentation/protocols/middleware'
+import { forbidden, serverError, success } from '@/presentation/helpers/http-helper'
+import { AccessDeniedError } from '@/presentation/errors/access-denied-error'
+import { HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { LoadAccountByToken } from '@/domain/use-cases/load-account-by-token'
+
+export class AuthMiddleware implements Middleware {
+  constructor (private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role?: string
+  ) {
+    this.loadAccountByToken = loadAccountByToken
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const token = httpRequest.cookies?.token
+      if (!token) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      const account = await this.loadAccountByToken.loadByToken(token, this.role)
+      if (!account) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      return success({ accountId: account._id })
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
