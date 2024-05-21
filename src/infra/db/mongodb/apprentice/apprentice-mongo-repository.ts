0b2c26d3b68b817364def6fdf00f853ca2f88909
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-21 14:17:25 +0100

    feat: implement ApprenticeMongoRepository loadById method

--- src/infra/db/mongodb/apprentice/apprentice-mongo-repository.ts
+++ src/infra/db/mongodb/apprentice/apprentice-mongo-repository.ts
@@ -2,14 +2,25 @@ import { ApprenticeInformationParams, RegisterApprenticeInformationRepository } 
 import { MongoHelper } from '../helpers/mongo-helper'
 import { ObjectId } from 'mongodb'
 import { DeleteApprenticeInformationByAccountIdRepository } from '@/data/protocols/db/delete-apprentice-information-by-account-id-repository'
+import { LoadApprenticeInformationByAccountIdRepository } from '@/data/protocols/db/load-apprentice-information-by-account-id-repository'
+import { ApprenticeModel } from '@/domain/models/apprentice-model'
 
-export class ApprenticeMongoRepository implements RegisterApprenticeInformationRepository, DeleteApprenticeInformationByAccountIdRepository {
+export class ApprenticeMongoRepository implements
+  RegisterApprenticeInformationRepository,
+  DeleteApprenticeInformationByAccountIdRepository,
+  LoadApprenticeInformationByAccountIdRepository {
   async register (apprenticeInformation: ApprenticeInformationParams, configOps?: object): Promise<void> {
     const collection = await MongoHelper.getCollection('apprentices')
     await collection.insertOne({
       ...apprenticeInformation,
       accountId: new ObjectId(apprenticeInformation.accountId)
     }, configOps)
+  }
+
+  async loadById (accountId: string): Promise<ApprenticeModel> {
+    const apprenticesCollection = await MongoHelper.getCollection('apprentices')
+    const apprenticeInfo = await apprenticesCollection.findOne({ accountId: new ObjectId(accountId) })
+    return MongoHelper.mapObjectId<ApprenticeModel>(apprenticeInfo)
   }
 
   async deleteById (accountId: string, transactionOps?: object): Promise<boolean> {
