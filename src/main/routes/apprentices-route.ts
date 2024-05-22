340ad3577f00adff087a7dba8b206abf9b119afa
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-22 19:31:41 +0100

    feat: implement auth privilege role based on apprentices route

--- src/main/routes/apprentices-route.ts
+++ src/main/routes/apprentices-route.ts
@@ -5,10 +5,11 @@ import { deleteApprenticeControllerFactory } from '../factories/controller/accou
 import { updateApprenticeInductionControllerFactory } from '../factories/controller/apprentice/update-apprentice-induction/update-apprentice-induction-controller-factory'
 import { apprenticeMiddleware } from '../middlewares/apprentice-middleware'
 import { updateApprenticeAssessmentControllerFactory } from '../factories/controller/apprentice/update-apprentice-assessment/update-apprentice-assessment-controller-factory'
+import { adminMiddleware } from '../middlewares/admin-middleware'
 
 export default (router: Router) => {
-  router.post('/apprentices', expressRouteAdapter(registerApprenticeAccountControllerFactory()))
-  router.delete('/apprentices/:id', expressRouteAdapter(deleteApprenticeControllerFactory()))
+  router.post('/apprentices', adminMiddleware, expressRouteAdapter(registerApprenticeAccountControllerFactory()))
+  router.delete('/apprentices/:id', adminMiddleware, expressRouteAdapter(deleteApprenticeControllerFactory()))
   router.put('/apprentice/induction', apprenticeMiddleware, expressRouteAdapter(updateApprenticeInductionControllerFactory()))
   router.patch('/apprentice/assessment', apprenticeMiddleware, expressRouteAdapter(updateApprenticeAssessmentControllerFactory()))
 }
