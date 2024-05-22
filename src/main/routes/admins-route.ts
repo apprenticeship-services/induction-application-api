e98b6271f993739c010cc0c5aec7e213ab15d9d6
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-22 19:40:15 +0100

    feat: implement auth privilege role based on admins route

--- src/main/routes/admins-route.ts
+++ src/main/routes/admins-route.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 import { expressRouteAdapter } from '../adapters/express/express-route-adapter'
 import { registerAdminAccountControllerFactory } from '../factories/controller/account/register-admin-account/register-admin-account-controller-factory'
 import { deleteAdminControllerFactory } from '../factories/controller/account/delete-admin-account/delete-admin-account-controller-factory'
+import { adminMiddleware } from '../middlewares/admin-middleware'
 
 export default (router: Router) => {
-  router.post('/admins', expressRouteAdapter(registerAdminAccountControllerFactory()))
-  router.delete('/admins/:id', expressRouteAdapter(deleteAdminControllerFactory()))
+  router.post('/admins', adminMiddleware, expressRouteAdapter(registerAdminAccountControllerFactory()))
+  router.delete('/admins/:id', adminMiddleware, expressRouteAdapter(deleteAdminControllerFactory()))
 }
