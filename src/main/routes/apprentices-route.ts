e2364b67e939fb273b7375d10d858d47ce6c9324
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-22 10:44:40 +0100

    feat: implement /apprentice/induction api to update induction status

--- src/main/routes/apprentices-route.ts
+++ src/main/routes/apprentices-route.ts
@@ -2,8 +2,11 @@ import { Router } from 'express'
 import { expressRouteAdapter } from '../adapters/express/express-route-adapter'
 import { registerApprenticeAccountControllerFactory } from '../factories/controller/account/register-apprentice-account/register-apprentice-account-controller-factory'
 import { deleteApprenticeControllerFactory } from '../factories/controller/account/delete-apprentice-account/delete-apprentice-account-controller-factory'
+import { updateApprenticeInductionControllerFactory } from '../factories/controller/apprentice-induction/update-apprentice-induction/update-apprentice-induction-controller-factory'
+import { apprenticeMiddleware } from '../middlewares/apprentice-middleware'
 
 export default (router: Router) => {
   router.post('/apprentices', expressRouteAdapter(registerApprenticeAccountControllerFactory()))
   router.delete('/apprentices/:id', expressRouteAdapter(deleteApprenticeControllerFactory()))
+  router.put('/apprentice/induction', apprenticeMiddleware, expressRouteAdapter(updateApprenticeInductionControllerFactory()))
 }
