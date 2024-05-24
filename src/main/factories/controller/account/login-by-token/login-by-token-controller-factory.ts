4b3a8d47b005aebc914284e068846393223a6398
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-24 10:45:59 +0100

    feat: create /me route to reconnect user that has access token on headers

---
+++ src/main/factories/controller/account/login-by-token/login-by-token-controller-factory.ts
@@ -0,0 +1,13 @@
+import { DbLoadAccountByToken } from '@/data/use-cases/db/account/db-load-account-by-token'
+import { JwtAdapter } from '@/infra/cryptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
+import env from '@/main/config/env'
+import { LoginByTokenController } from '@/presentation/controllers/account/login/login-by-token-controller'
+import { Controller } from '@/presentation/protocols'
+
+export const loginByTokenControllerFactory = (): Controller => {
+  const decrypter = new JwtAdapter(env.jwtSecretToken)
+  const loadAccountByIdRepository = new AccountMongoRepository()
+  const loadAccountByToken = new DbLoadAccountByToken(decrypter, loadAccountByIdRepository)
+  return new LoginByTokenController(loadAccountByToken)
+}
--- src/main/routes/login-route.ts
+++ src/main/routes/login-route.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { expressRouteAdapter } from '../adapters/express/express-route-adapter'
 import { loginControllerFactory } from '../factories/controller/account/login/login-controller-factory'
+import { loginByTokenControllerFactory } from '../factories/controller/account/login-by-token/login-by-token-controller-factory'
 
 export default (router: Router) => {
   router.post('/login', expressRouteAdapter(loginControllerFactory()))
+  router.get('/me', expressRouteAdapter(loginByTokenControllerFactory()))
 }
