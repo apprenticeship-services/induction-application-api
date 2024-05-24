0f67ef41ac1aaaa6e1b9f5d67b80aea01c346fa6
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-24 14:39:05 +0100

    fix: bug fix on Bcrypt salt param from .env to Number type

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,7 +2,7 @@ export default {
   nodeEnvironment: process.env.NODE_ENV || 'development',
   mongoUri: process.env.MONGO_URL || 'mongodb://localhost:27017/cetb-node-api',
   port: process.env.PORT || 4000,
-  salt: process.env.SALT || 12,
+  salt: Number(process.env.SALT) || 12,
   jwtSecretToken: process.env.JSON_SECRET_TOKEN || '8DB3B6393AA5BC15120CCE32A610EFB8B88DCF6E27D68F681C53D48894D44B2C',
   emailService: process.env.MAIL_SERVICE || 'Outlook365',
   emailServiceUser: process.env.MAIL_SERVICE_EMAIL || 'cetb_apprenticeship_service@outlook.com',
