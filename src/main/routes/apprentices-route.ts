b75a2c4b4b95e276189be9062358fdd31964b9b6
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-25 21:08:21 +0100

    feat: create /apprentices route to fetch apprentices

--- src/main/routes/apprentices-route.ts
+++ src/main/routes/apprentices-route.ts
@@ -6,9 +6,11 @@ import { updateApprenticeInductionControllerFactory } from '../factories/control
 import { apprenticeMiddleware } from '../middlewares/apprentice-middleware'
 import { updateApprenticeAssessmentControllerFactory } from '../factories/controller/apprentice/update-apprentice-assessment/update-apprentice-assessment-controller-factory'
 import { adminMiddleware } from '../middlewares/admin-middleware'
+import { getApprenticesByDateRangeControllerFactory } from '../factories/controller/apprentice/get-apprentices-by-date-range/get-apprentices-by-date-range-controller-factory'
 
 export default (router: Router) => {
   router.post('/apprentices', adminMiddleware, expressRouteAdapter(registerApprenticeAccountControllerFactory()))
+  router.get('/apprentices', adminMiddleware, expressRouteAdapter(getApprenticesByDateRangeControllerFactory()))
   router.delete('/apprentices/:id', adminMiddleware, expressRouteAdapter(deleteApprenticeControllerFactory()))
   router.put('/apprentice/induction', apprenticeMiddleware, expressRouteAdapter(updateApprenticeInductionControllerFactory()))
   router.patch('/apprentice/assessment', apprenticeMiddleware, expressRouteAdapter(updateApprenticeAssessmentControllerFactory()))
