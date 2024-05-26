8763fb6ac2f0290d473d7c40ef628a3f7b5dc8ed
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-26 12:34:27 +0100

    test: create LogMongoRepository to save errors on database

---
+++ src/infra/db/mongodb/log/log-mongo-repository.spec.ts
@@ -0,0 +1,27 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log-mongo-repository'
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+  })
+  afterEach(async () => {
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create a error log on success ', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_stack_trace')
+    const countDocs = await errorCollection.countDocuments()
+    expect(countDocs).toBe(1)
+  })
+})
